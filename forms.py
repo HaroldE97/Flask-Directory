a8d4d669cd82cd7e5a3658b2df324b3a06304d39
Harold Espinoza <haroldesptru@gmail.com>
2018-10-31 21:03:18 -0600

    Añadidos los campos que contendrá el formulario de contacto

---
+++ forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, HiddenField
+from wtforms.fields.html5 import EmailField, TelField
+from wtforms.validators import DataRequired, length, Email
+
+
+errors = {
+    'name_len': 'Ingrese un nombre de contacto entre 4 y 25 carácteres',
+    'email': 'Ingrese un correo electronico valido'
+}
+
+
+class Person(FlaskForm):
+    id = HiddenField('')
+    name = StringField('Nombre Completo', [DataRequired(), length(min=4, max=25, message=errors['name_len'])])
+    email = EmailField('Correo electronico', [DataRequired(), Email(errors['email'])])
+    direccion = TextAreaField('Domicilio', [DataRequired()])
+    telefono = TelField('Número teléfonico', [DataRequired(), length(min=8)])
+    submit = SubmitField('Enviar')
